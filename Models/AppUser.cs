5c504bf484e2100d6d770340641462670eecbd0a
washington786 <dkmawasha@gmail.com>
2025-08-29 16:16:47 +0200

    models

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,8 +1,10 @@
 using System;
+using Microsoft.AspNetCore.Identity;
 
 namespace TaskManager.Models;
 
-public class AppUser
+public class AppUser : IdentityUser
 {
-
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
 }
--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,8 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskManager.Models;
 
 public class TaskItem
 {
+    public int Id { get; set; }
 
+    [Required]
+    [MaxLength(150)]
+    public string Title { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+
+    public string UserId { get; set; } = string.Empty;
+
+    public AppUser? User { get; set; }
 }
